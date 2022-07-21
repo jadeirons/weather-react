64b353cc00e643aba7fb8daadc2fab32ec72cd5b
jadeirons <105827385+jadeirons@users.noreply.github.com>
2022-07-20 22:05:37 -0400

    Added Forecast

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./Styles.css";
 import Search from "./Search";
 import Weather from "./Weather";
 import Footer from "./Footer";
+import Forecast from "./Forecast";
 
 export default function App() {
   return (
     <div class="Box">
       <Search />
       <Weather />
+      <Forecast />
       <Footer />
     </div>
   );
