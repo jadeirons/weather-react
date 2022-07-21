498de682384c7209b4cddf91ddb0d3a8d49d846c
jadeirons <105827385+jadeirons@users.noreply.github.com>
2022-07-21 09:22:19 -0400

    Search bar updates

--- src/Search.js
+++ src/Search.js
@@ -4,11 +4,7 @@ export default function Search() {
   return (
     <div class="Search">
       <form class="search-form">
-        <input
-          type="search"
-          placeholder="Search for a city..."
-          class="search"
-        />
+        <input type="search" class="search" />
         <input type="submit" value="Search" class="button" />
       </form>
     </div>
