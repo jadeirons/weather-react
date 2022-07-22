8c02634a0d9a21be48f125fe6a57c5794df18542
jadeirons <105827385+jadeirons@users.noreply.github.com>
2022-07-22 12:57:56 -0400

    Moved image inside weather section

--- src/Weather.js
+++ src/Weather.js
@@ -12,48 +12,46 @@ export default function Search() {
         </div>
       </div>
 
-      <div class="row main">
+      <div class="row weather-section">
         <div class="col-7">
-          {" "}
-          <div class="weather-section">
-            <div class="row">
-              <div class="col-8">
-                <span class="current-temperature">
-                  <i id="icon" class="fa-solid fa-cloud"></i> 24
-                  <span class="unit">°C</span>
-                </span>
-              </div>
-
-              <div class="col-4">
-                <div class="high-low">
-                  <div class="current-high">
-                    HIGH: <span id="current-high">33</span>°C
-                  </div>
-                  LOW: <span id="current-low">21</span>°C
-                </div>
-              </div>
+          <div class="row">
+            <div class="col-8">
+              <span class="current-temperature">
+                <i id="icon" class="fa-solid fa-cloud"></i> 24
+                <span class="unit">°C</span>
+              </span>
             </div>
 
-            <div class="row">
-              <div class="col-4 weather-detail one">
-                <p>
-                  Feels like: <div class="weather-stat">26°C</div>
-                </p>
-              </div>
-
-              <div class="col-4 weather-detail two">
-                <p>
-                  Humidity: <div class="weather-stat">50%</div>
-                </p>
-              </div>
-              <div class="col-4 weather-detail three">
-                <p>
-                  Wind: <div class="weather-stat">6km/hr</div>
-                </p>
+            <div class="col-4">
+              <div class="high-low">
+                <div class="current-high">
+                  HIGH: <span id="current-high">33</span>°C
+                </div>
+                LOW: <span id="current-low">21</span>°C
               </div>
             </div>
           </div>
+
+          <div class="row">
+            <div class="col-4 weather-detail one">
+              <p>
+                Feels like: <div class="weather-stat">26°C</div>
+              </p>
+            </div>
+
+            <div class="col-4 weather-detail two">
+              <p>
+                Humidity: <div class="weather-stat">50%</div>
+              </p>
+            </div>
+            <div class="col-4 weather-detail three">
+              <p>
+                Wind: <div class="weather-stat">6km/hr</div>
+              </p>
+            </div>
+          </div>
         </div>
+
         <div class="col-5">
           <img class="weather-image" src="/images/cloudy.png" alt="cloudy" />
         </div>
