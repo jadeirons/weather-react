1140a4998cd8e2eda60d7b87d94e9865866eae33
jadeirons <105827385+jadeirons@users.noreply.github.com>
2022-08-27 08:20:47 -0400

    Added min/max temp to forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,66 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./styles/Forecast.css";
 
-export default function Forecast() {
-  return (
-    <div className="Forecast">
-      <div className="row forecast-section">
-        <div className="forecast-box col-2 one">
-          <div className="row forecast-day">Monday</div>
-          <div className="row">
-            <i className="fa-solid fa-cloud forecast-icon"></i>
-          </div>
-          <div className="row">
-            <div className="forecast-high-low">
-              <span className="temperature-high">33°</span> | 21°
+export default function Forecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
+  function handleForecastResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="Forecast">
+        <div className="row forecast-section">
+          <div className="forecast-box col-2 one">
+            <div className="row forecast-day">Monday</div>
+            <div className="row">
+              <i className="fa-solid fa-cloud forecast-icon"></i>
+            </div>
+            <div className="row">
+              <div className="forecast-high-low">
+                <span className="temperature-high">
+                  {Math.round(forecast[0].temp.max)}°
+                </span>{" "}
+                | {Math.round(forecast[0].temp.min)}°
+              </div>
             </div>
           </div>
-        </div>
-        <div className="forecast-box col-2 two">
-          <div className="row forecast-day">Tuesday</div>
-          <div className="row">
-            <i className="fa-solid fa-cloud forecast-icon"></i>
-          </div>
-          <div className="row">
-            <div className="forecast-high">
-              <span className="temperature-high">33°</span> | 21°
+          <div className="forecast-box col-2 two">
+            <div className="row forecast-day">Tuesday</div>
+            <div className="row">
+              <i className="fa-solid fa-cloud forecast-icon"></i>
+            </div>
+            <div className="row">
+              <div className="forecast-high">
+                <span className="temperature-high">33°</span> | 21°
+              </div>
             </div>
           </div>
-        </div>
-        <div className="forecast-box col-2 one">
-          <div className="row forecast-day">Wednesday</div>
-          <div className="row">
-            <i className="fa-solid fa-cloud forecast-icon"></i>
-          </div>
-          <div className="row">
-            <div className="forecast-high-low">
-              <span className="temperature-high">33°C</span> | 21°
+          <div className="forecast-box col-2 one">
+            <div className="row forecast-day">Wednesday</div>
+            <div className="row">
+              <i className="fa-solid fa-cloud forecast-icon"></i>
+            </div>
+            <div className="row">
+              <div className="forecast-high-low">
+                <span className="temperature-high">33°C</span> | 21°
+              </div>
             </div>
           </div>
-        </div>
-        <div className="forecast-box col-2 two">
-          <div className="row forecast-day">Thursday</div>
-          <div className="row">
-            <i className="fa-solid fa-cloud forecast-icon"></i>
-          </div>
-          <div className="row">
-            <div className="forecast-high-low">
-              <span className="temperature-high">33°</span> | 21°
+          <div className="forecast-box col-2 two">
+            <div className="row forecast-day">Thursday</div>
+            <div className="row">
+              <i className="fa-solid fa-cloud forecast-icon"></i>
+            </div>
+            <div className="row">
+              <div className="forecast-high-low">
+                <span className="temperature-high">33°</span> | 21°
+              </div>
             </div>
           </div>
-        </div>
-        <div className="forecast-box col-2 one">
-          <div className="row forecast-day">Friday</div>
-          <div className="row">
-            <i className="fa-solid fa-cloud forecast-icon"></i>
-          </div>
-          <div className="row">
-            <div className="forecast-high-low">
-              <span className="temperature-high">33°</span> | 21°
+          <div className="forecast-box col-2 one">
+            <div className="row forecast-day">Friday</div>
+            <div className="row">
+              <i className="fa-solid fa-cloud forecast-icon"></i>
+            </div>
+            <div className="row">
+              <div className="forecast-high-low">
+                <span className="temperature-high">33°</span> | 21°
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "1979bc82187db3756ece8eeb6f265da0";
+    const unit = "metric";
+    const lat = props.lat;
+    const lon = props.lon;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
+    axios.get(apiUrl).then(handleForecastResponse);
+  }
 }
